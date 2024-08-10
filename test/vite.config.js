6e361b8c546988307f3dd2139c7655299ab819b8
Mamalakisgr <charzevg@students.kastoria.teiwm.gr>
2024-08-11 00:02:29 +0300

    vite

--- test/vite.config.js
+++ test/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: './', // Ensures correct path resolution in production
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
