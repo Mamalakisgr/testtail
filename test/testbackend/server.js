34aa59b95e0b6a6fa31e8c7cf8586d69d62ed300
Mamalakisgr <charzevg@students.kastoria.teiwm.gr>
2024-08-11 04:34:35 +0300

    rereee

--- test/testbackend/server.js
+++ test/testbackend/server.js
@@ -16,18 +16,7 @@ const bcrypt = require('bcrypt');
 const allowedOrigins = ['http://localhost:5173', 'https://main--dapper-beijinho-216f7a.netlify.app'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin like mobile apps or curl requests
-    if (!origin) return callback(null, true);
-    
-    // Check if the origin is in the allowedOrigins array
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    
-    return callback(null, true);
-  },
+  origin: 'https://main--dapper-beijinho-216f7a.netlify.app', // Allow requests from Netlify
   credentials: true,
 }));
 app.use(bodyParser.json());
