a312f14bc0d7987f5b40f74a3e97322f739a2e57
Mamalakisgr <charzevg@students.kastoria.teiwm.gr>
2024-08-11 04:17:29 +0300

    ere

--- test/vite.config.js
+++ test/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { backendUrl } from '@/js/index'; // Adjust the path if necessary
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,7 +22,7 @@ export default defineConfig({
     port:5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:5174', // Use environment variable
+        target: backendUrl || 'http://localhost:5174', // Use environment variable
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
