abd3ea0ea301a28ca0c164070ad05974ed9ce7fd
Mamalakisgr <charzevg@students.kastoria.teiwm.gr>
2024-09-15 14:57:17 +0300

    fix

--- test/src/js/index.js
+++ test/src/js/index.js
@@ -40,7 +40,7 @@ export const fetchCartItems = async () => {
     console.error('Failed to fetch cart items', error);
   }
 };
-export const backendUrl = 'http://localhost:5174';
+export const backendUrl = 'https://testtail-xs2f.vercel.app';
 
 watch(() => (newVal) => {
   if (newVal) {
